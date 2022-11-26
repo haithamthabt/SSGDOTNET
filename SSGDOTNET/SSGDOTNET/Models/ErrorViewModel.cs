f6890ff345671691e7454290738ff901b79b77d3
Haitham Thabt <haithamthabt@gmail.com>
2022-11-26 01:14:53 -0800

    Created MVC Project

---
+++ SSGDOTNET/SSGDOTNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SSGDOTNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
